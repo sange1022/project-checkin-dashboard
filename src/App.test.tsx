3f5e4b8fd64d62dcea84eee151cdb632307fbc3f
sange1022 <raefnip967@gmail.com>
2026-08-26 16:44:42 +0800

    feat: add floor plan maker shortcut

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -74,6 +74,7 @@ test.each([
   ['GoGoScrum', 'https://gogoscrum.com'],
   ['公众号编辑器', 'https://sange1022.github.io/xuwu-wechat-editor/'],
   ['图片拼贴', 'https://sange1022.github.io/xuwu-image-collage/'],
+  ['平面图制作', 'https://sange1022.github.io/floor-plan-maker/'],
   ['构', 'https://sange1022.github.io/qf-07-9a6c3e21/'],
   ['间', 'https://sange1022.github.io/random-planar-composition/'],
   ['海', 'https://sange1022.github.io/contour-text-studio/?v=5787e7a'],
